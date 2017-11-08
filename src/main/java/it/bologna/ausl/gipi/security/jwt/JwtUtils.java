8d0013ed49fadd6f027e8f83727e6da265abab23
a.marcomini <devnull@localhost>
2017-11-08 14:15:16 +0100

    aggiunto package JWT

---
+++ src/main/java/it/bologna/ausl/gipi/security/jwt/JwtUtils.java
@@ -0,0 +1,15 @@
+package it.bologna.ausl.gipi.security.jwt;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtUtils {
+
+    @Value("${jwt.secret}")
+    private static String SECRET_KEY;
+
+    public static String getSECRET_KEY() {
+        return SECRET_KEY;
+    }
+}
