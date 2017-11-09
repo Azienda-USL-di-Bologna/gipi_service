f7678eb8caaa16a373c707a2c9cbaa8fb0bee8d1
g.demarco <devnull@localhost>
2017-11-09 09:54:50 +0100

    cambiati url di risposta

--- src/main/java/it/bologna/ausl/gipi/security/jwt/UserController.java
+++ src/main/java/it/bologna/ausl/gipi/security/jwt/UserController.java
@@ -23,7 +23,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/gipi/user")
 public class UserController {
 
     private static final Logger logger = LoggerFactory.getLogger(UserController.class);
