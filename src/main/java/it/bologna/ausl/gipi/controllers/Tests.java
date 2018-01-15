1cbfd8e19763123ebad362870e4050a64b41d276
a.marcomini <devnull@localhost>
2018-01-15 14:31:42 +0100

    update test class

--- src/main/java/it/bologna/ausl/gipi/controllers/Tests.java
+++ src/main/java/it/bologna/ausl/gipi/controllers/Tests.java
@@ -5,9 +5,21 @@
  */
 package it.bologna.ausl.gipi.controllers;
 
+import com.fasterxml.jackson.annotation.JsonView;
+import com.querydsl.jpa.EclipseLinkTemplates;
+import com.querydsl.jpa.JPQLQuery;
+import com.querydsl.jpa.impl.JPAQuery;
+import it.bologna.ausl.entities.baborg.Azienda;
+import it.bologna.ausl.entities.baborg.Pec;
+import it.bologna.ausl.entities.baborg.QAzienda;
+import it.bologna.ausl.entities.baborg.QPec;
+import it.bologna.ausl.entities.baborg.View;
 import it.bologna.ausl.entities.gipi.Fase;
 import it.bologna.ausl.entities.gipi.Iter;
+import it.bologna.ausl.entities.gipi.Procedimento;
+import it.bologna.ausl.entities.gipi.QProcedimento;
 import it.bologna.ausl.gipi.exceptions.GipiRequestParamsException;
+import it.bologna.ausl.gipi.odata.complextypes.StrutturaCheckTipoProcedimento;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,8 +28,19 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import it.bologna.ausl.gipi.process.Process;
 import java.text.ParseException;
+import java.util.List;
+import java.util.Properties;
+import javax.persistence.EntityGraph;
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
 
 /**
  *
@@ -29,6 +52,9 @@ public class Tests {
 
     @Autowired
     Process process;
+
+    @Autowired
+    EntityManager em;
 
 //    @RequestMapping(value = "getNextFase", method = RequestMethod.GET)
 //    public ResponseEntity getNextFase() {
@@ -63,6 +89,59 @@ public class Tests {
 
         process.stepOn(iter, null, null);
         return new ResponseEntity(HttpStatus.OK);
+    }
+
+    @JsonView(View.Summary.class)
+    @RequestMapping(value = "dammi", method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE, "application/hal+json"})
+    public ResponseEntity dammi() throws ParseException, GipiRequestParamsException {
+//        List<Procedimento> p1 = em.createQuery(
+//                "select p "
+//                + "from Procedimento p "
+//                + "where p.id = :idProcedimento", Procedimento.class)
+//                .setParameter("idProcedimento", 580)
+//                .getResultList();
+        EntityGraph<Azienda> aziendaEntityGraph = em.createEntityGraph(Azienda.class);
+        aziendaEntityGraph.addAttributeNodes("pecList");
+
+//        QProcedimento qProcedimento = QProcedimento.procedimento;
+        JPQLQuery<Azienda> query = new JPAQuery(em);
+
+//        Procedimento p = query
+//                .from(qProcedimento)
+//                .where(qProcedimento.id.eq(8))
+//                .fetchOne();
+        QAzienda qAzienda = QAzienda.azienda;
+        QPec qPec = QPec.pec;
+
+        List<Azienda> aziende = em.createQuery(
+                "select a "
+                + "from Azienda a "
+                + "where a.id = :idAzienda", Azienda.class)
+                //.setHint("javax.persistence.fetchgraph", aziendaEntityGraph)
+                .setParameter("idAzienda", 5)
+                .getResultList();
+
+//        Azienda azienda = query.select(qAzienda)
+//                .from(qAzienda)
+//                .where(qAzienda.id.eq(5)).fetchFirst();
+//        List<Pec> pecList = azienda.getPecList();
+//        pecList.stream().forEach(p -> {
+//            System.out.println(p.getIndirizzo());
+//        });
+//        azienda.setPecList(pecList);
+//        Azienda azienda = em.createQuery(
+//                "select a "
+//                + "from Azienda a "
+//                + "where a.id = :idAzienda", Azienda.class)
+//                .setParameter("idAzienda", 2)
+//                .getSingleResult();
+//        try (Connection con = sql2o.open()) {
+//            List<Procedimento> p = con.createQuery(queryStruttureText)
+//                    .addParameter("idProcedimento", 580)
+//                    .addColumnMapping("id_struttura", "idStruttura")
+//                    .executeAndFetch(Procedimento.class);
+//        }
+        return new ResponseEntity(aziende.get(0), HttpStatus.OK);
     }
 
 }
