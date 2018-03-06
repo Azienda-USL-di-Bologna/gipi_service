d5aea899c1a85d16e386b0624d6842ff469d91e3
g.demarco <devnull@localhost>
2018-03-06 11:10:21 +0100

    cambiato url base

--- src/main/java/it/bologna/ausl/gipi/controllers/utilityController.java
+++ src/main/java/it/bologna/ausl/gipi/controllers/utilityController.java
@@ -5,11 +5,15 @@
  */
 package it.bologna.ausl.gipi.controllers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import it.bologna.ausl.entities.gipi.AziendaTipoProcedimento;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,6 +30,9 @@ public class utilityController {
     private String revision;
     @Value("${modificationTime}")
     private String modificationTime;
+    
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @RequestMapping(value = "version", method = RequestMethod.GET)
     public ResponseEntity<String> getVersion() {
@@ -37,6 +44,14 @@ public class utilityController {
     public String getCaca() {
         return revision + "\n" + modificationTime;
         //return new ResponseEntity<>(revision + "\n" + modificationTime, HttpStatus.OK);
+    }
+    
+    @RequestMapping(value = "save", method = RequestMethod.POST, consumes = "application/json;charset=UTF-8")
+    public void save(@RequestBody AziendaTipoProcedimento entity) {
+        
+//        AziendaTipoProcedimento aziendaTipoProcedimento = objectMapper.convertValue(entity, AziendaTipoProcedimento.class);
+        
+        System.out.println("aziendaTipoProcedimento: " + entity);
     }
 
 }
