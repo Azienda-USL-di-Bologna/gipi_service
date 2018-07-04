532e43bdee414707ee29db24633ac1a17cb70337
a.marcomini <devnull@localhost>
2018-07-04 15:12:39 +0200

    insertimento di setDataInizioRun e fine di JobInviaNotificheTerminiSospensioneIterScaduti

--- src/main/java/it/bologna/ausl/gipi/config/scheduler/jobs/JobInviaNotificheTerminiSospensioneIterScaduti.java
+++ src/main/java/it/bologna/ausl/gipi/config/scheduler/jobs/JobInviaNotificheTerminiSospensioneIterScaduti.java
@@ -48,18 +48,18 @@ import org.springframework.stereotype.Component;
  *
  * @author Salo
  */
-
 @Component
 public class JobInviaNotificheTerminiSospensioneIterScaduti implements BaseScheduledJob {
+
     private static final Logger log = LoggerFactory.getLogger(JobInviaNotificheTerminiSospensioneIterScaduti.class);
     private static JSONArray megaJsonArray;
-    
+
     @Value("${inviaNotificheWebApi}")
     private String inviaNotificheWebApiPath;
-    
+
     @PersistenceContext
     EntityManager em;
-    
+
     @Autowired
     ObjectMapper objectMapper;
 
@@ -68,10 +68,10 @@ public class JobInviaNotificheTerminiSospensioneIterScaduti implements BaseSched
 
     @Autowired
     ServiceManager serviceManager;
-    
+
     @Autowired
     AziendaRepository aziendaRepository;
-    
+
     QAzienda qAzienda = QAzienda.azienda;
     QIter qIter = QIter.iter;
     QEventoIter qEventoIter = QEventoIter.eventoIter;
@@ -80,10 +80,10 @@ public class JobInviaNotificheTerminiSospensioneIterScaduti implements BaseSched
     public String getJobName() {
         return "invia_notifiche_termini_sospensione_iter_scaduti";
     }
-    
+
     public void callBabel(int idAzienda, JSONArray ja) throws ParseException {
         String functionName = "callBabel";
-        if(ja.size() > 0){
+        if (ja.size() > 0) {
             try {
                 String urlChiamata = GetBaseUrls.getBabelSuiteWebApiUrl(idAzienda, em, objectMapper) + inviaNotificheWebApiPath;
                 JSONObject jo = new JSONObject();
@@ -118,27 +118,29 @@ public class JobInviaNotificheTerminiSospensioneIterScaduti implements BaseSched
             }
         }
     }
-    
-    public JSONObject getJsonOfThisIter(Iter iter){
+
+    public JSONObject getJsonOfThisIter(Iter iter) {
         JSONObject o = new JSONObject();
         List<String> utenti = new ArrayList<String>();
-        try{
+        try {
             utenti.add(iter.getIdResponsabileProcedimento().getIdPersona().getCodiceFiscale());
-        }catch(Error e){
+        } catch (Error e) {
             log.error("Non è stato trovato il responsabile del procedimento dell'iter " + iter.getId().toString());
         }
         try {
-            if(iter.getIdProcedimento().getIdResponsabileAdozioneAttoFinale() != null && !utenti.contains(iter.getIdProcedimento().getIdResponsabileAdozioneAttoFinale().getIdPersona().getCodiceFiscale()))
+            if (iter.getIdProcedimento().getIdResponsabileAdozioneAttoFinale() != null && !utenti.contains(iter.getIdProcedimento().getIdResponsabileAdozioneAttoFinale().getIdPersona().getCodiceFiscale())) {
                 utenti.add(iter.getIdProcedimento().getIdResponsabileAdozioneAttoFinale().getIdPersona().getCodiceFiscale());
-        }catch (Error e) {
-            log.error("Non è stato trovato il responsabile adozione dell'atto finale dell'iter " + iter.getId().toString() 
+            }
+        } catch (Error e) {
+            log.error("Non è stato trovato il responsabile adozione dell'atto finale dell'iter " + iter.getId().toString()
                     + " dal procedimento " + iter.getIdProcedimento().getId().toString());
         }
         try {
-            if(iter.getIdProcedimento().getIdTitolarePotereSostitutivo() != null && !utenti.contains(iter.getIdProcedimento().getIdTitolarePotereSostitutivo().getIdPersona().getCodiceFiscale()))
+            if (iter.getIdProcedimento().getIdTitolarePotereSostitutivo() != null && !utenti.contains(iter.getIdProcedimento().getIdTitolarePotereSostitutivo().getIdPersona().getCodiceFiscale())) {
                 utenti.add(iter.getIdProcedimento().getIdTitolarePotereSostitutivo().getIdPersona().getCodiceFiscale());
-        }catch(Error e){
-            log.error("Non è stato trovato il titolare del potere esecutivo dell'iter " + iter.getId().toString() 
+            }
+        } catch (Error e) {
+            log.error("Non è stato trovato il titolare del potere esecutivo dell'iter " + iter.getId().toString()
                     + " dal procedimento " + iter.getIdProcedimento().getId().toString());
         }
         o.put("cfUtenti", utenti);
@@ -147,45 +149,45 @@ public class JobInviaNotificheTerminiSospensioneIterScaduti implements BaseSched
         o.put("descrizioneNotifica", "EsaurimentoTempiSospensione");
         return o;
     }
-    
+
     public void puttaQuestoIterNelGiustoJsonAziendale(Iter iter) {
         String functionName = "puttaQuestoIterNelGiustoJsonAziendale";
-        megaJsonArray.forEach(item->{
+        megaJsonArray.forEach(item -> {
             JSONObject o = (JSONObject) item;
             int idazienda = (int) o.get("idAzienda");
-            
-            if(idazienda == iter.getIdProcedimento().getIdAziendaTipoProcedimento().getIdAzienda().getId()){
+
+            if (idazienda == iter.getIdProcedimento().getIdAziendaTipoProcedimento().getIdAzienda().getId()) {
                 JSONArray ja = (JSONArray) o.get("datiDaMandare");
                 ja.add(getJsonOfThisIter(iter));
-            }     
-        });       
+            }
+        });
     }
-    
+
     public void caricaGliIterSospesiAndPopolaIlMegaJson() {
         String functionName = "caricaGliIterSospesiAndPopolaIlMegaJson";
         log.info(functionName + " query ricerca iter sospesi");
         List<Iter> lista = new ArrayList<>();
-        JPQLQuery<Iter> query = new JPAQuery(em, EclipseLinkTemplates.DEFAULT);      
+        JPQLQuery<Iter> query = new JPAQuery(em, EclipseLinkTemplates.DEFAULT);
         lista = query.select(this.qIter)
                 .from(this.qIter)
                 .where(
                         this.qIter.idStato.codice.eq(Stato.CodiciStato.SOSPESO.toString())
-                        .and(this.qIter.giorniSospensioneTrascorsi.isNotNull())
+                                .and(this.qIter.giorniSospensioneTrascorsi.isNotNull())
                 )
                 .fetch();
         log.info(functionName + " iter sospesi totali " + lista.size());
-        lista.forEach(iter->{
+        lista.forEach(iter -> {
             int giorniSospensioneTrascorsi = iter.getGiorniSospensioneTrascorsi() != null ? iter.getGiorniSospensioneTrascorsi() : 0;
             int derogaSospensione = iter.getDerogaSospensione() != null ? iter.getDerogaSospensione() : 0;
             int durataMassimaSospensione = iter.getIdProcedimento().getIdAziendaTipoProcedimento().getDurataMassimaSospensione() != null ? iter.getIdProcedimento().getIdAziendaTipoProcedimento().getDurataMassimaSospensione() : 0;
-            if(giorniSospensioneTrascorsi > derogaSospensione + durataMassimaSospensione){
+            if (giorniSospensioneTrascorsi > derogaSospensione + durataMassimaSospensione) {
                 log.info(functionName + " da notificare l'iter con id = " + iter.getId());
                 puttaQuestoIterNelGiustoJsonAziendale(iter);
             }
         });
     }
-    
-     public JSONObject gimmeJsonAziendale(int idAzienda) {
+
+    public JSONObject gimmeJsonAziendale(int idAzienda) {
         JSONObject o = new JSONObject();
         o.put("idAzienda", idAzienda);
         o.put("datiDaMandare", new JSONArray());
@@ -200,42 +202,45 @@ public class JobInviaNotificheTerminiSospensioneIterScaduti implements BaseSched
         ServiceKey serviceKey = new ServiceKey(getJobName(), null);
         Servizio service = serviceManager.getService(serviceKey);
         if (service != null && service.getActive()) {
-            log.info(functionName + "--> sono entrato nel main del servizio di notifica per Iter Sospesi");        
+            serviceManager.setDataInizioRun(serviceKey);
+            log.info(functionName + "--> sono entrato nel main del servizio di notifica per Iter Sospesi");
             List<Azienda> aziende = new ArrayList<>();
-            megaJsonArray = new JSONArray(); 
+            megaJsonArray = new JSONArray();
             JPQLQuery<Iter> query = new JPAQuery(em, EclipseLinkTemplates.DEFAULT);
-            log.info(functionName +  " eseguo query per il recupero delle aziende");
+            log.info(functionName + " eseguo query per il recupero delle aziende");
             aziende = query.select(this.qAzienda).from(this.qAzienda).fetch();
-            log.info(functionName +  " lista aziende: ");
-            aziende.forEach(item->log.info(functionName + "> " + item.getId() + "\t" + item.getDescrizione()));
+            log.info(functionName + " lista aziende: ");
+            aziende.forEach(item -> log.info(functionName + "> " + item.getId() + "\t" + item.getDescrizione()));
 
-            log.info(functionName +  " preparazione del megaJsonArray ");
+            log.info(functionName + " preparazione del megaJsonArray ");
             // per ogni azienda mi prendo gli iter ed i loro dati per le notifiche mettendoli nel megaJsonArray
-            aziende.forEach(item->{
+            aziende.forEach(item -> {
                 megaJsonArray.add(gimmeJsonAziendale(item.getId()));
             });
 
             caricaGliIterSospesiAndPopolaIlMegaJson();
 
-             // ORA DOVREI AVERE IL MEGAJSONARRAY POPOLATO: ciclare e chiamare Babel per ogni azienda
+            // ORA DOVREI AVERE IL MEGAJSONARRAY POPOLATO: ciclare e chiamare Babel per ogni azienda
             System.out.println("*****************************************************************");
             System.out.println("*****************************************************************");
             log.info(functionName + "--> ITER DA NOTIFICARE TROVATI:");
-            megaJsonArray.forEach(item->log.info(item.toString()));
+            megaJsonArray.forEach(item -> log.info(item.toString()));
             System.out.println("*****************************************************************");
             System.out.println("********************* LET'S CALL BABEL **************************");
-            megaJsonArray.forEach(item->{
+            megaJsonArray.forEach(item -> {
                 JSONObject o = (JSONObject) item;
                 int idazienda = (int) o.get("idAzienda");
                 JSONArray ja = (JSONArray) o.get("datiDaMandare");
-                if (ja.size() > 0)
+                if (ja.size() > 0) {
                     try {
                         callBabel(idazienda, ja);
-                } catch (ParseException ex) {
-                    java.util.logging.Logger.getLogger(JobInviaNotificheTerminiSospensioneIterScaduti.class.getName()).log(Level.SEVERE, null, ex);
+                    } catch (ParseException ex) {
+                        java.util.logging.Logger.getLogger(JobInviaNotificheTerminiSospensioneIterScaduti.class.getName()).log(Level.SEVERE, null, ex);
+                    }
                 }
             });
-        }else{
+            serviceManager.setDataFineRun(serviceKey);
+        } else {
             log.info(getJobName() + ": servizio non attivo");
         }
     } // fine del run
